6d0996dfe1bd08f6bf694e36bd24a8b5806a750b
PiotrDS <01180852@pw.edu.pl>
2026-01-05 15:16:47 +0100

    Add augmentation feature

--- src/etl_data.py
+++ src/etl_data.py
@@ -3,46 +3,93 @@ import numpy as np
 import nibabel as nib
 import SimpleITK as sitk
 from torch.utils.data import Dataset
+import torch
 import matplotlib.pyplot as plt
 import cv2
+import albumentations as A
+
+def load_img_and_mask(img_path, mask_path, des_size):
+
+    img = sitk.ReadImage(img_path)
+    img = sitk.DICOMOrient(img, "RAS")
+    img = sitk.GetArrayFromImage(img)
+
+    mask = sitk.ReadImage(mask_path)
+    mask = sitk.DICOMOrient(mask, "RAS")
+    mask = sitk.GetArrayFromImage(mask)
+                
+    img = cv2.resize(img, dsize =des_size, interpolation=cv2.INTER_CUBIC )
+    mask = cv2.resize(mask, dsize =des_size, interpolation=cv2.INTER_CUBIC )
+
+    z = img.shape[2] // 2
+
+    img = img[:,:,z]
+    mask = mask[:,:,z]
+
+    img = standardize(img)
+    mask = standardize(mask)
+
+    return img, mask
+
+def standardize(img, eps=1e-8):
+
+    pixels = img[img > 0]
+
+    mean = pixels.mean()
+    std = pixels.std()
+
+    img = (img - mean) / (std + eps)
+    return img
 
 class spine_dataset(Dataset):
     
     def __init__(self):
         self.samples_img = []
         self.samples_mask = []
+        self.augmented = []
 
         des_size = (256,256)
 
         path_1_images = f"..{os.sep}data{os.sep}data_1{os.sep}images"
         path_1_masks = f"..{os.sep}data{os.sep}data_1{os.sep}masks"
 
-        for image, mask in zip(sorted(os.listdir(path_1_images)), sorted(os.listdir(path_1_masks))):
+        transform = A.Compose([
+                        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=15, p=1.0),
+                        A.GaussNoise(p=0.2),
+                        A.ElasticTransform(alpha=1, sigma=50, p=0.3)
+        ], additional_targets={'mask': 'mask'})
+
+        for image, mask in zip(sorted(os.listdir(path_1_images))[:15], sorted(os.listdir(path_1_masks))[:15]):
             full_image_path = os.path.join(path_1_images, image)
             full_mask_path = os.path.join(path_1_masks, mask)
 
             try:
 
-                img = sitk.GetArrayFromImage(sitk.ReadImage(full_image_path))
-                mask = sitk.GetArrayFromImage(sitk.ReadImage(full_mask_path))
-                
-                img = cv2.resize(img, dsize =des_size, interpolation=cv2.INTER_CUBIC )
-                mask = cv2.resize(mask, dsize =des_size, interpolation=cv2.INTER_CUBIC )
-
-                z = img.shape[2] // 2
-
-                img = img[:,:,z]
-                mask = mask[:,:,z]
-
+                img, mask = load_img_and_mask(full_image_path, full_mask_path, des_size)
                 self.samples_img.append(img)
                 self.samples_mask.append(mask)
+                self.augmented.append(False)
+
+                for _ in range(8):
+                    try:
+                        augmented_img_mask = transform(image=img, mask=mask)
+                    except Exception as e:
+                        continue
+                        
+                    aug_img = augmented_img_mask["image"]
+                    aug_mask = augmented_img_mask["mask"]
+                        
+                    self.samples_img.append(aug_img)
+                    self.samples_mask.append(aug_mask)
+                    self.augmented.append(True)
+
             except Exception as e:
                 print(e)
                 print("couldn't load ", full_image_path, " or ", full_mask_path)
             
         path_2 = f"..{os.sep}data{os.sep}data_2"
 
-        for el in sorted(os.listdir(path_2)):
+        for el in sorted(os.listdir(path_2))[:15]:
             full_path = os.path.join(path_2, el)
 
             full_image_path = os.path.join(full_path, f"{el}_sagittal_image.nii.gz")
@@ -50,23 +97,25 @@ class spine_dataset(Dataset):
 
             try:
 
-                img = sitk.GetArrayFromImage(sitk.ReadImage(full_image_path))
-                mask = sitk.GetArrayFromImage(sitk.ReadImage(full_mask_path))
-                
-                img = np.moveaxis(img, 0, -1)
-                mask = np.moveaxis(mask, 0,-1)
-                
-                img = cv2.resize(img, dsize =des_size, interpolation=cv2.INTER_CUBIC )
-                mask = cv2.resize(mask, dsize =des_size, interpolation=cv2.INTER_CUBIC )
-
-                z = img.shape[2] // 2
-
-                img = img[:,:,z]
-                mask = mask[:,:,z]
+                img, mask = load_img_and_mask(full_image_path, full_mask_path, des_size)
 
                 self.samples_img.append(img)
                 self.samples_mask.append(mask)
-            
+                self.augmented.append(False)
+
+                for _ in range(5):
+                    try:
+                        augmented_img_mask = transform(image=img, mask=mask)
+                    except Exception as e:
+                        continue
+
+                    aug_img = augmented_img_mask["image"]
+                    aug_mask = augmented_img_mask["mask"]
+                        
+                    self.samples_img.append(aug_img)
+                    self.samples_mask.append(aug_mask)
+                    self.augmented.append(True)
+
             except Exception as e:
                 
                 print(e)
@@ -89,8 +138,11 @@ class spine_dataset(Dataset):
     def visualize(self, idx):
         img = self.samples_img[idx]
         mask = self.samples_mask[idx]
+        ori = self.augmented[idx]
 
         fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+        fig.suptitle(f"Visualization of idx={idx} original = {not(ori)}")
 
         axes[0].imshow(img, cmap='gray')
         axes[0].set_title('Image')
